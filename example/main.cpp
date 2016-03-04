3fe464c8133705ec8247df40c2ffbc7f5753c97b
新井昇平 <shohei.arai.52@gmail.com>
2016-03-04 23:40:50 +0900

    add: matrix example

--- example/main.cpp
+++ example/main.cpp
@@ -1,24 +1,26 @@
 //--------------------------------------------------//
-// File Name: Arith.h                               //
-// Function: arithmetic calculation                 //
+// File Name: main.cpp                              //
+// Function: Arithmetic calculation                 //
 // Copyright(C) 2016 shoarai                        //
 // The MIT License (MIT)                            //
 //--------------------------------------------------//
 #include "../src/arith.h"
+#include <stdio.h>
 
 using namespace arith;
 
 void calcVetor();
+void calcMatrix();
 
 int main()
 {
     calcVetor();
-
+    calcMatrix();
   	return 0;
 }
 
 void calcVetor () {
-    // Initialize
+    // Initialize x, y and z elements
     Vector vecA;
     Vector vecB(1, 10, 100);
 
@@ -26,9 +28,9 @@ void calcVetor () {
     vecA.set(1, 2, 3);
 
     // Get elements
-    double x = vecA.getx();	// x axis
-    double y = vecA.gety();	// y axis
-    double z = vecA.getz();	// z axis
+    double x = vecA.getx();
+    double y = vecA.gety();
+    double z = vecA.getz();
 
     Vector vecC;
 
@@ -46,7 +48,7 @@ void calcVetor () {
     vecC  = vecA / val;
     vecC /= val;
 
-    // // Cross product
+    // Cross product
     vecC = vecA * vecB;
 
     // Inner product
@@ -57,4 +59,40 @@ void calcVetor () {
 
     // Normalization
     vecC = vecA.normalize();
+}
+
+void calcMatrix() {
+    // Initialize the number of row and columns
+    Matrix matA(3, 2);
+    Matrix matB(3, 2);
+
+    // Set elements
+    matA(0, 1) = 10;
+    matB(2, 2) = 20;
+
+    // Get elements
+    double a01 = matA(0, 1);
+    double b22 = matB(2, 2);
+
+    // Addition and subtraction of matrixes
+    Matrix matC = matA + matB;
+    matC += matA;
+    matC  = matA - matB;
+    matC -= matB;
+
+    Matrix matD(2, 3);
+
+    // Multiplication and division of matrixes
+    Matrix matE = matD * matA;
+
+    int val = 10;
+
+    // Multiplication and division of a matrix and a scholar
+    matA  = matB * val;
+    matA *= val;
+    matA  = matB / val;
+    matA /= val;
+
+    // Transpose
+    matA = matD.transpose();
 }
